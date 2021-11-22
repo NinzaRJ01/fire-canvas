87a1a5d0cb1c6730ad91df64882d393e4d52affc
NinzaRJ01 <rajatagni432@gmail.com>
2021-11-22 22:46:28 +0530

    Intialized White Board Component

---
+++ components/WhiteBoard.py
@@ -0,0 +1,86 @@
+# Author : Rajat 
+# File Contains Code for WhiteBoard Component Of Fire Canvas Project
+# -----Consist Of : Scrible Area, ImageView 
+# Important Methods :
+# Open cv R1 event handler*
+# Open cv R2 event Handler*
+# MousePress event Handler | Done
+# MouseMove event Handler | Done
+# MouseRelase event Handler |Done
+# Paint Event  | Done
+# Refernces:
+#   https://forum.qt.io/topic/99106/qwidget-paintengine-should-no-longer-be-called
+#   https://www.notion.so/Errors-And-Solution-8f526f6873fd49df9048725fd92ab3a2
+#   https://github.com/baoboa/pyqt5/blob/master/examples/widgets/scribble.py
+
+import PyQt5.QtCore as qtc
+import PyQt5.QtGui as qtg
+import PyQt5.QtWidgets as qtw
+
+class WhiteBoard(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0,0,404,403)
+        self.modified = False
+        self.scribbling = False # used to handle scribbling
+        self.myPenWidth = 1
+        self.myPenColor = qtc.Qt.blue
+        self.lastPoint = qtc.QPoint()
+        self.image = qtg.QImage()
+    
+    def mousePressEvent(self, event):
+        if event.button() == qtc.Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.scribbling = True
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & qtc.Qt.LeftButton) and self.scribbling:
+            self.drawLineTo(event.pos())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() ==qtc.Qt.LeftButton and self.scribbling:
+            self.drawLineTo(event.pos())
+            self.scribbling = False
+    def drawLineTo(self, endPoint):
+        # Draw Line(not straight) From lastPoint to end Point
+        painter = qtg.QPainter(self.image)
+        painter.setPen(qtg.QPen(self.myPenColor, self.myPenWidth, qtc.Qt.SolidLine,
+                qtc.Qt.RoundCap, qtc.Qt.RoundJoin))
+        painter.drawLine(self.lastPoint, endPoint)
+        self.modified = True
+
+        rad = int(self.myPenWidth / 2 + 2)
+        self.update(qtc.QRect(self.lastPoint, endPoint).normalized().adjusted(-rad, -rad, +rad, +rad))
+        self.lastPoint = qtc.QPoint(endPoint)
+    def paintEvent(self, event):
+        painter = qtg.QPainter(self)
+        dirtyRect = event.rect()
+        painter.drawImage(dirtyRect, self.image, dirtyRect)
+    ## ResizeEvent trigger when width is resized
+    def resizeEvent(self, event):
+        # Method Calls Resize Image Method To Rsize the Image >= Size Of Window
+        if self.width() > self.image.width() or self.height() > self.image.height():
+            newWidth = max(self.width() + 128, self.image.width())
+            newHeight = max(self.height() + 128, self.image.height())
+            self.resizeImage(self.image, qtc.QSize(newWidth, newHeight))
+            self.update()
+
+        super(WhiteBoard, self).resizeEvent(event)
+
+    def resizeImage(self, image, newSize):
+        #Resizes Image to New Size
+        if image.size() == newSize:
+            return
+
+        newImage = qtg.QImage(newSize, qtg.QImage.Format_RGB32)
+        newImage.fill(qtg.qRgb(255, 255, 255))
+        painter = qtg.QPainter(newImage)
+        painter.drawImage(qtc.QPoint(0, 0), image)
+        self.image = newImage
+# main thread
+if __name__ == "__main__":
+    import sys
+    app = qtw.QApplication(sys.argv)
+    win = WhiteBoard()
+    win.show()
+    app.exec_()
